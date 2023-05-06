68c1768f64225296436bb735085b77679f6ae426
Pugga <38046181+GoodEgUY@users.noreply.github.com>
2023-05-07 00:32:44 +0300

    Practice 1.1 Loaded

---
+++ practice-1.1.py
@@ -0,0 +1,9 @@
+num = int(input("Введіть число: "))
+if num > 0: 
+    print(f"Введене число {num} додатне.")
+elif num == 0:
+    print(f"Введене число {num} дорівнює нулю.")
+else:
+    print(f"Введене число {num} від'ємне.")
+
+#Pugachev Ilya
